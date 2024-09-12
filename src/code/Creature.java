11da838508aa3ffd1a3876dcd0b84fd0be95d1f7
Bryson Lindy <bslindy@gmail.com>
2024-09-12 09:45:42 -0700

    Populated Creature class with details

--- src/code/Creature.java
+++ src/code/Creature.java
@@ -1,3 +1,26 @@
 public class Creature
 {
+    private static final int CREATURE_DEFAULT_HP = 100;
+
+    private final String name;
+    private final Date   dateOfBirth;
+    private int          healthPoints;
+
+    public Creature(final String name, final Date dateOfBirth)
+    {
+        validateName(name);
+
+        this.name          = name;
+        this.dateOfBirth   = dateOfBirth;
+        this.healthPoints  = CREATURE_DEFAULT_HP;
+    }
+
+    private static void validateName(final String name)
+    {
+        if(name == null || name.isEmpty())
+        {
+            throw new IllegalArgumentException("Creature name cannot be null or empty");
+        }
+    }
+
 }
