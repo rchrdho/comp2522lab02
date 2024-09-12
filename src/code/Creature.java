addbef9df6b5b748e8bd7e089c3ef558c8345e60
Bryson Lindy <bslindy@gmail.com>
2024-09-12 10:14:24 -0700

    finished Creature class. finished DamageException, HealingException

--- src/code/Creature.java
+++ src/code/Creature.java
@@ -1,6 +1,6 @@
 public class Creature
 {
-    private static final int CREATURE_DEFAULT_HP = 100;
+    private static final int CREATURE_MAX_HP = 100;
     private static final int CREATURE_MIN_HP     = 0;
 
     private final String name;
@@ -13,7 +13,7 @@ public class Creature
 
         this.name          = name;
         this.dateOfBirth   = dateOfBirth;
-        this.healthPoints  = CREATURE_DEFAULT_HP;
+        this.healthPoints  = CREATURE_MAX_HP;
     }
 
     private static void validateName(final String name)
@@ -43,6 +43,40 @@ public class Creature
             throw new DamageException("Damage cannot be negative");
         }
 
+    }
+
+    private void heal(final int healAmount)
+    {
+        this.healthPoints += healAmount;
+
+        if(this.healthPoints > CREATURE_MAX_HP)
+        {
+            this.healthPoints = CREATURE_MAX_HP;
+        }
+
+        if(healAmount < 0)
+        {
+            throw new HealingException("Heal amount must be positive");
+        }
+    }
+
+    public int getAgeYears()
+    {
+        return this.dateOfBirth.getYear() - this.dateOfBirth.getMonth();
+    }
+
+    public void getDetails()
+    {
+        final StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append(this.name);
+        sb.append(" HP: ");
+        sb.append(this.healthPoints);
+        sb.append(" Years: ");
+        sb.append(this.getAgeYears());
+        System.out.println(sb);
     }
 
 }
