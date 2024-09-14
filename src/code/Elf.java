bbc0f5b6b0dd350aaecbfbdab0d97d7387857e18
phyotkha <phyotkha@outlook.com>
2024-09-13 20:38:25 -0700

    Continued with Elf class

--- src/code/Elf.java
+++ src/code/Elf.java
@@ -1,61 +1,146 @@
+/**
+ * Elf class represents an Elf, which is a subclass of the Creature class.
+ * Elf class has additional attributes such as mana, its own health, and can cast spells and restore mana.
+ *
+ * @author Phyo, Bryson, Richard
+ * @version 1.0
+ */
 public class Elf extends Creature
 {
-    //TODO: Constructor (String name, Date dateOfBirth, int health, int mana)
-    //TODO: Override getDetails() : Add mana to output
-    //TODO: castSpell() Reduces mana by 5/Deals 10 damage to creature/If mana < 5, throw a checked LowManaException
-    //TODO: restoreMana(int amount): Increases mana but cannot exceed 50
+    // TODO: To revisit comments
+    private static final int ELF_MIN_MANA          = 0;
+    private static final int ELF_MAX_MANA          = 50;
+    private static final int ELF_MAX_HP            = 80;
+    private static final int ELF_MIN_HP            = 0;
+    private static final int ELF_MANA_REDUCTION    = 5;
+    private static final int ELF_TARGET_DAMAGE     = 10;
+    private static final int ELF_LOWEST_MANA_USAGE = 5;
 
-    private static final int ELF_MIN_MANA = 0;
-    private static final int ELF_MAX_MANA = 50;
+    private int mana;
+    private int elfHP;
 
-    private static final int ELF_DEFAULT_HP = 80;
-    private static final int ELF_MIN_HP = 0;
-
-    private static final int ELF_MANA_REDUCTION  = 5;
-    private static final int CREATURE_DAMAGE_NUM = 10;
-
-    private static final int ELF_LOWEST_MANA = 5;
-
-    private int elfMana;
-    private int elfHealth;
-
-    public Elf(final String name,
-               final Date   dateOfBirth,
-               final int    elfHealth,
-               final int    elfMana)
+    /**
+     * Constructs an Elf with the specified name, date of birth, health, and mana.
+     *
+     * @param elfName      name of the elf
+     * @param elfBirthDate birthdate of the elf
+     * @param elfHP        health points of the elf (should be between ELF_MIN_HP and ELF_MAX_HP)
+     * @param mana         mana points of the elf (should be between ELF_MIN_MANA and ELF_MAX_MANA)
+     */
+    public Elf(final String elfName,
+               final Date   elfBirthDate,
+               final int    elfHP,
+               final int    mana)
     {
-        super(name, dateOfBirth);
+        super(elfName, elfBirthDate);
 
-        validateMana(elfMana);
+        validateHealth(elfHP);
+        validateMana(mana);
 
-        this.elfMana   = elfMana;
-        this.elfHealth = elfHealth;
+        this.elfHP = elfHP;
+        this.mana   = mana;
     }
 
-    private static void validateMana(final int elfMana)
+    private static void validateMana(final int mana)
     {
-        if(elfMana < ELF_MIN_MANA || elfMana > ELF_MAX_MANA)
+        if(mana < ELF_MIN_MANA || mana > ELF_MAX_MANA)
         {
-            throw new IllegalArgumentException(String.format("Elf Mana is less than %d or higher than %d ",
+            throw new IllegalArgumentException(String.format("Elf mana cannot be less than %d or higher than %d ",
                     ELF_MIN_MANA, ELF_MAX_MANA));
         }
     }
 
-    //TODO: castSpell() to check logic
-    public void castSpell() throws LowManaException {
-
-        this.elfMana -= ELF_MANA_REDUCTION;
-
-        if(this.elfMana < ELF_LOWEST_MANA)
+    private static void validateHealth(final int elfHP)
+    {
+        if(elfHP < ELF_MIN_HP || elfHP > ELF_MAX_HP)
         {
-            throw new LowManaException(String.format("Mana is lower than %d. Cannot use it right now.",
-                    ELF_LOWEST_MANA));
+            throw new IllegalArgumentException(String.format("Elf health cannot be less than %d or higher than %d",
+                    ELF_MIN_HP, ELF_MAX_HP));
         }
     }
 
+    /**
+     * Casts a spell on the target creature, reducing the elf's mana by ELF_MANA_REDUCTION and dealing ELF_TARGET_DAMAGE
+     * damage to the target creature. Throws a LowManaException when elf's mana reaches ELF_LOWEST_MANA_USAGE
+     *
+     * @param target Creature to receive damage
+     * @throws LowManaException If the elf's mana is less than ELF_LOWEST_MANA_USAGE
+     */
+    public void castSpell(final Creature target)
+            throws LowManaException
+    {
+        validateManaUsage(this.mana);
+
+        this.mana -= ELF_MANA_REDUCTION;
+
+        attack(target);
+    }
+
+    private void validateManaUsage(final int mana)
+            throws LowManaException
+    {
+        if (mana < ELF_LOWEST_MANA_USAGE)
+        {
+            throw new LowManaException(String.format("Mana is lower than %d. Cannot use it right now.",
+                    ELF_LOWEST_MANA_USAGE));
+        }
+    }
+
+    /**
+     * Override attack method in Creature Class.
+     *
+     * @param target Creature to receive damage
+     */
+    @Override
+    public void attack(final Creature target)
+    {
+        super.attack(target);
+        setHealthPoints(-ELF_TARGET_DAMAGE);
+
+    }
+
+    /**
+     * Restores the elf's mana by the specified amount, ensuring that the mana does not exceed ELF_MAX_MANA
+     *
+     * @param amount the amount of mana to restore
+     */
+    public void restoreMana(final int amount)
+    {
+        validateRestoreManaAmount(amount);
+
+        mana += amount;
+    }
 
 
+    // TODO: To check with the boys; allows amount to exceed max mana?
+    private static void validateRestoreManaAmount(final int amount)
+    {
+        if (amount < ELF_MIN_MANA)
+        {
+            throw new IllegalArgumentException(String.format("Mana restore amount cannot be less than %d",
+                    ELF_MIN_MANA));
+        }
 
+        if (amount > ELF_MAX_MANA)
+        {
+            throw new IllegalArgumentException(String.format("Mana restore amount cannot be more than %d",
+                    ELF_MAX_MANA));
+        }
+    }
 
+    /**
+     * Override getDetails method in Creature class by adding mana to the output.
+     */
+    @Override
+    public void getDetails()
+    {
+        final StringBuilder sb;
+        sb = new StringBuilder();
 
+        super.getDetails();
+
+        sb.append("Mana: ");
+        sb.append(mana);
+        System.out.println(sb);
+    }
 }
